9d3166bde8d22d4e256bcd6d0a24b2dc72a21a71
kareem muhammad <kareem.gaad@gmail.com>
2021-11-19 02:49:21 +0200

    .

--- src/components/MintComponent/MintComponent.tsx
+++ src/components/MintComponent/MintComponent.tsx
@@ -14,7 +14,7 @@ const MintComponent = () => {
         <h1>Connect</h1>
         <Mint />
         {/* <button >MINT</button> */}
-        <p>Pre-Sale price: 0.55 Solana</p>
+        <p>Pre-Sale price: 0.75 Solana</p>
         <img src={Solana} alt="" />
       </div>
     </div>
